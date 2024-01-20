76d6ad7a8b652ec2387eaf7645e8525ab36fc501
Simon Alexander <bcspace8@gmail.com>
2024-01-20 14:19:01 +1300

    Loaded Mock Serial Ports From Rust

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+use slint::{SharedString, VecModel};
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
-    
+    let ports: Vec<SharedString> = vec!["Example 1", "String 2", "String 3"]
+        .into_iter()
+        .map(|s| SharedString::from(s))
+        .collect();
+    let ports_model = std::rc::Rc::new(VecModel::from(ports));
+    ui.set_serial_ports(ports_model.clone().into());
+
     ui.run()
 }

8fd6439422572c69507928d3de5514243c6fdfa4
Simon Alexander <simon.alexander@fsae.co.nz>
2024-01-21 23:47:41 +1300

    Added Hashmap Storage

--- src/can_frame.rs
+++ src/can_frame.rs
@@ -1,7 +1,9 @@
+use std::collections::HashMap;
 use std::rc::Rc;
 use slint::{ModelRc, SharedString, StandardListViewItem, VecModel};
 use rand::random;
 
+#[derive(Clone)]
 pub struct CANFrame {
     pub can_id: String,
     pub byte_1_data: u8,
@@ -16,18 +18,44 @@ pub struct CANFrame {
     pub count: u8,
 }
 
+pub struct CANFrameStorage {
+    frames: HashMap<String, CANFrame>,
+}
+
+impl CANFrameStorage {
+    pub fn new() -> CANFrameStorage {
+        CANFrameStorage {
+            frames: HashMap::new(),
+        }
+    }
+
+    pub fn update_or_insert(&mut self, frame: CANFrame) {
+        self.frames.insert(frame.can_id.clone(), frame.clone());
+    }
+
+    pub fn get(&self, can_id: String) -> Option<&CANFrame> {
+        self.frames.get(&can_id)
+    }
+
+    pub fn gather_all_models(&self) -> Vec<ModelRc<StandardListViewItem>> {
+        self.frames.values()
+            .map(|frame| frame.to_model_rc())
+            .collect()
+    }
+}
+
 impl CANFrame {
     pub fn to_model_rc(&self) -> ModelRc<StandardListViewItem> {
         let vec = vec![
             StandardListViewItem::from(SharedString::from(self.can_id.clone())),
-            StandardListViewItem::from(SharedString::from(self.byte_1_data.to_string())),
-            StandardListViewItem::from(SharedString::from(self.byte_2_data.to_string())),
-            StandardListViewItem::from(SharedString::from(self.byte_3_data.to_string())),
-            StandardListViewItem::from(SharedString::from(self.byte_4_data.to_string())),
-            StandardListViewItem::from(SharedString::from(self.byte_5_data.to_string())),
-            StandardListViewItem::from(SharedString::from(self.byte_6_data.to_string())),
-            StandardListViewItem::from(SharedString::from(self.byte_7_data.to_string())),
-            StandardListViewItem::from(SharedString::from(self.byte_8_data.to_string())),
+            StandardListViewItem::from(SharedString::from(format!("0x{:02x}", self.byte_1_data))),
+            StandardListViewItem::from(SharedString::from(format!("0x{:02x}", self.byte_2_data))),
+            StandardListViewItem::from(SharedString::from(format!("0x{:02x}", self.byte_3_data))),
+            StandardListViewItem::from(SharedString::from(format!("0x{:02x}", self.byte_4_data))),
+            StandardListViewItem::from(SharedString::from(format!("0x{:02x}", self.byte_5_data))),
+            StandardListViewItem::from(SharedString::from(format!("0x{:02x}", self.byte_6_data))),
+            StandardListViewItem::from(SharedString::from(format!("0x{:02x}", self.byte_7_data))),
+            StandardListViewItem::from(SharedString::from(format!("0x{:02x}", self.byte_8_data))),
             StandardListViewItem::from(SharedString::from(self.timestamp.to_string())),
             StandardListViewItem::from(SharedString::from(self.count.to_string())),
         ];
--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,28 @@ mod can_frame;
 use std::rc::Rc;
 use can_frame::CANFrame;
 use slint::{ModelRc, VecModel};
+use crate::can_frame::CANFrameStorage;
 
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
+    let mut frame_storage = CANFrameStorage::new();
+
+    // Fake Frames
+    frame_storage.update_or_insert(CANFrame::generate_random(Some("100")));
+    frame_storage.update_or_insert(CANFrame::generate_random(Some("123")));
+    frame_storage.update_or_insert(CANFrame::generate_random(None));
+    frame_storage.update_or_insert(CANFrame::generate_random(None));
+    frame_storage.update_or_insert(CANFrame::generate_random(None));
+    frame_storage.update_or_insert(CANFrame::generate_random(None));
+    frame_storage.update_or_insert(CANFrame::generate_random(None));
+
     let ports_model = Rc::new(VecModel::from(serial::get_serial_ports()));
     ui.set_serial_ports(ports_model.clone().into());
 
-    let table_model = ModelRc::from(Rc::new(VecModel::from(vec![CANFrame::generate_random(Some("100")).to_model_rc(), CANFrame::generate_random(Some("123")).to_model_rc(), CANFrame::generate_random(None).to_model_rc(), CANFrame::generate_random(None).to_model_rc(), CANFrame::generate_random(None).to_model_rc(), CANFrame::generate_random(None).to_model_rc(), CANFrame::generate_random(None).to_model_rc()])));
+    let table_model = ModelRc::from(Rc::new(VecModel::from(frame_storage.gather_all_models())));
     ui.set_table_data(table_model);
     ui.run()
 }
